76e5959f399ac022524218a8ead5e6f242056194
G00433295 <145337306+G00433295@users.noreply.github.com>
2026-09-24 11:21:57 +0100

    Update age mapping logic in ex2d.js
    Modify age mapping to double ages under 70 while keeping others unchanged.

--- ex2d.js
+++ ex2d.js
@@ -1,10 +1,14 @@
+//Creates an array of ages
 let ages = [3,451,341,4];
 
+//Goes through each age
 let myArray = ages.map(
     (age) => {
+        //Doubles Ages under 70
         if(age < 70){
             return age*2;
         }else{
+            //Keeps the other ages the same
             return age;
         }
     }
