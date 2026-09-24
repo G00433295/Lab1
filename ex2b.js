54bbf9fe5b8a72e54710d4150d069a7a0fecf28f
G00433295 <145337306+G00433295@users.noreply.github.com>
2026-09-24 11:21:09 +0100

    Create arrow function to log myVal
    Added an arrow function to log a message to the console.

--- ex2b.js
+++ ex2b.js
@@ -1,5 +1,7 @@
+//Creates an arrow function with myVal as a parameter
 let message = (myVal) => {
 
+    // Displays the Value of myVal in the console
     console.log(myVal);
 }
 message("Hello, World!")
